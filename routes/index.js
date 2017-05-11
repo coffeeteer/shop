acaa35c6feb66c3aa26d3a7beeee7b2bd6bacb5c
Coffeeteer <tgrieshiemer@gmail.com>
2017-05-11 18:11:09 -0400

    working on the issues

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ var Product = require('../models/product.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log('Here');
 	Product.find(function(err, docs){
+		console.log('here 2');
 		var productChunks = [];
 		chunkSize = 3;
+		console.log('here 3');
 		for(var i = 0; i < docs.length; i += chunkSize){
-			productChunks.push(docs.slice(i, i + chunkSize));
+			console.log('here 4');
+			productChunks.push(docs.slice(i, i + chunkSize, console.log('here 5')));
 		}
 		res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
 	});
