2e3fbb0859f2b8365d50d8fb021ceb6be3061173
Coffeeteer <tgrieshiemer@gmail.com>
2017-05-18 22:57:41 -0400

    added validation

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var index = require('./routes/index');
 //require('./seed/product-seeder.js');
@@ -31,6 +32,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'theSecret', resave: false, saveUninitialized: false}));
 app.use(flash());
