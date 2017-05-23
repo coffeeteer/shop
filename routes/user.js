d0d2f137d444e0dbfc29900a5442461a09f493ce
Coffeeteer <tgrieshiemer@gmail.com>
2017-05-23 15:38:53 -0400

    made some routes that connected to header including signin, signout, and logout

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,19 @@ var passport = require('passport');
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
+router.get('/profile', isLoggedIn, function(req, res, next) {
+	res.render('user/profile');
+});
+
+router.get('/logout', isLoggedIn, function(req, res, next){
+	req.logout();
+	res.redirect('/');
+});
+
+router.use('/', notLoggedIn, function(req, res, next) {
+	next();
+});
+
 router.get('/signup', function(req, res, next){
 	var messages = req.flash('error');
 	res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
@@ -28,18 +41,17 @@ router.post('/signin', passport.authenticate('local-signin', {
 	failureFlash: true
 }));
 
-router.get('/logout', function(req, res, next){
-
-});
-
-router.get('/profile', isLoggedIn, function(req, res, next) {
-	res.render('user/profile');
-});
-
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/');
+}
+
+function notLoggedIn(req, res, next) {
+	if(!req.isAuthenticated()){
 		return next();
 	}
 	res.redirect('/');
